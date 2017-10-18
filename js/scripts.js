32dcaec6a58746147e9b5ff0399dc8e9e693d496
rwart <32432397+rwart@users.noreply.github.com>
2017-10-18 18:06:19 +0200

    ready for review

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,15 @@
-
+document.getElementById('js-addElem').addEventListener('click', function (e) {
+  var list = document.getElementById('js-list');
+  var element = document.createElement('li');
+  element.innerHTML = 'item ' + list.getElementsByTagName('li').length;
+  list.appendChild(element);
+});
+/*
+document.getElementById('js-addElem').addEventListener('click', function (e) {
+  var list = document.getElementById('js-list');
+  var element = document.createElement('li');
+  var node = document.createTextNode('item ' + list.getElementsByTagName('li').length);
+  element.appendChild(node);
+  list.appendChild(element);
+});
+*/
